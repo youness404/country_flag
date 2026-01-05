826417584c86021f6479cab4bf30b2c90050a6d1
Youness Horra <104984275+youness404@users.noreply.github.com>
2026-01-05 12:44:34 +0100

    Replace API key in fetch request
    Updated API key for country flag fetch request.

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ async function fetchCountryFlag(country) {
         const response = await fetch(`https://api.api-ninjas.com/v1/countryflag?country=${country.code}`,{
             method : 'GET',
             headers : {
-                'X-Api-Key': 'YOUR_API_KEY'
+                'X-Api-Key': 'CGiPIIxMhkPodmZh4PQveg==OMqP4AwOlIvRwRqZ'
             }
         });
         const data = await response.json();
